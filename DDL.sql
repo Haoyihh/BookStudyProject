6be98041108822575b772e592a79e57822d51927
dwij <dwij0712@gmail.com>
2024-11-04 02:27:47 -0500

    Create, alter and grant

--- DDL.sql
+++ DDL.sql
@@ -12,3 +12,28 @@ BEGIN
    END;
 END;
 /
+-- Create the new user with a secure password
+CREATE USER PROJECT IDENTIFIED BY "BookmySpace#";
+
+-- Set default tablespace and quota for the new user
+ALTER USER PROJECT DEFAULT TABLESPACE users QUOTA UNLIMITED ON users;
+
+-- Set temporary tablespace for the new user
+ALTER USER PROJECT TEMPORARY TABLESPACE TEMP;
+
+-- Grant basic connection and session privileges
+GRANT CONNECT TO PROJECT;
+
+-- Grant the necessary object and session creation privileges
+GRANT CREATE SESSION, CREATE VIEW, CREATE TABLE, ALTER SESSION, CREATE SEQUENCE TO PROJECT;
+
+-- Grant additional administrative and resource privileges
+-- Grant only specific roles if needed. 'RESOURCE' is a broad privilege and includes unnecessary permissions.
+GRANT CREATE SYNONYM, CREATE DATABASE LINK TO PROJECT;
+
+-- Optionally, grant the 'UNLIMITED TABLESPACE' privilege
+-- Use cautiously to avoid excessive resource usage by the user.
+GRANT UNLIMITED TABLESPACE TO PROJECT;
+
+-- End of script
+/
